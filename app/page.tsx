5cad29ec84bacdb41656083b4e7fffa851f1e1e0
Vihaan <vverma270705@gmail.com>
2024-06-22 02:12:22 +0530

    debugging

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import Consulting from "@/components/Consulting";
 import Development from "@/components/Development";
 import Heading from "@/components/Heading";
 import HeroSection from "@/components/HeroSection";
-import Loader from "@/components/Loader";
 import Training from "@/components/Training";
 import Windows from "@/components/Windows";
 import Image from "next/image";
@@ -14,7 +13,6 @@ export default function Home() {
   const consultingRef = useRef<HTMLImageElement>(null);
   const devRef = useRef<HTMLImageElement>(null);
   const trainRef = useRef<HTMLImageElement>(null);
-  const [show, setShow] = useState(false);
   const [refsLoaded, setRefsLoaded] = useState(
     Boolean(
       consultingRef.current?.getBoundingClientRect() &&
@@ -24,16 +22,11 @@ export default function Home() {
   );
   console.log(
     "🚀 ~ Home ~ consultingRef:",
+    refsLoaded,
     consultingRef.current,
     consultingRef.current?.getBoundingClientRect()
   );
   const [isSm, setIsSm] = useState(false);
-  useEffect(() => {
-    const id = setTimeout(() => {
-      setShow(true);
-    }, 250);
-    return () => clearTimeout(id);
-  }, []);
   useEffect(() => {
     if (
       !refsLoaded &&
@@ -46,12 +39,10 @@ export default function Home() {
     consultingRef.current?.getBoundingClientRect,
     devRef.current?.getBoundingClientRect,
     trainRef.current?.getBoundingClientRect,
-    show,
   ]);
   useEffect(() => {
     setIsSm(window?.innerWidth < 768);
   }, []);
-  if (!show) return <Loader />;
   return (
     <div className="flex flex-col no-scrollbar flex-1">
       {refsLoaded && (
