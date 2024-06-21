6dfbbfac00d47fef8f5cedf189ac2abd20974fe0
Vihaan <vverma270705@gmail.com>
2024-06-22 02:24:49 +0530

    refresh bug fixed

--- app/page.tsx
+++ app/page.tsx
@@ -20,6 +20,7 @@ export default function Home() {
 
   useEffect(() => {
     setIsSm(window?.innerWidth < 768);
+    window.scrollTo(0, 0);
   }, []);
   return (
     <div className="flex flex-col no-scrollbar flex-1">
