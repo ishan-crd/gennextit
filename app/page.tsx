736c094de4646f2bc14d24f09dfbdcc96aa6ed45
Vihaan <vverma270705@gmail.com>
2024-06-22 02:35:02 +0530

    scroll bug fixed

--- app/page.tsx
+++ app/page.tsx
@@ -20,9 +20,11 @@ export default function Home() {
 
   useEffect(() => {
     setIsSm(window?.innerWidth < 768);
-    // window.scrollTo(0, 0);
-    document.documentElement.scrollTop = 0;
   }, []);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    document.documentElement.scrollTop = 0;
+  }, [refsLoaded]);
   return (
     <div className="flex flex-col no-scrollbar flex-1">
       {refsLoaded && (
